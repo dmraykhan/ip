86e1e5c212648d99d5297c475fdeb9782f7a45ed
Aykhan Damirli <aykhan@u.nus.edu>
2026-08-30 04:33:35 +0800

    Complete Level-0 rename, greeting, and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Twizzy User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Twizzy.java
@@ -0,0 +1,24 @@
+/**
+ * Runs the Twizzy chatbot's greeting and farewell sequence.
+ */
+public class Twizzy {
+    public static void main(String[] args) {
+        String divider = "____________________________________________________________";
+        String banner = " _______        _                     \n"
+                + "|__   __|      (_)                    \n"
+                + "   | |_      ___ __________   _       \n"
+                + "   | \\ \\ /\\ / / |_  /_  / | | |      \n"
+                + "   | |\\ V  V /| |/ / / /| |_| |      \n"
+                + "   |_| \\_/\\_/ |_/___/___| \\__, |      \n"
+                + "                           __/ |      \n"
+                + "                          |___/";
+
+        System.out.println(divider);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Twizzy.");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
+    }
+}
